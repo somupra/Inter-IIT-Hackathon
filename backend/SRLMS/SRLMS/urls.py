72dcfc21ec5488d101298c175b4f8f6cf2d3c41c
Somu Prajapati <somupra9@gmail.com>
2019-12-20 11:08:23 +0530

    Authentication up, Env keys not setup yet

---
+++ backend/SRLMS/SRLMS/urls.py
@@ -0,0 +1,20 @@
+from django.conf import settings
+from django.contrib import admin
+from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf.urls import url
+# from allauth.account.views import confirm_email as allauthemailconfirmation
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('auth/', include('rest_auth.urls')),
+    path('auth/registration/', include('rest_auth.registration.urls')),
+]
+
+
+"""
+Strictly for the development mode, Django will serve the files from the server for that purpose, media root will be bounded to `MEDIA_ROOT` defined in the settings. For production mode, Nginx will be used.
+"""
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
